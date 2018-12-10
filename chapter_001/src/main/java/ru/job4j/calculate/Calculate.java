f639912818ac249b4afaf04d624e7fb272353022
Andrey Takmazian <takmazian@mail.ru>
2018-12-10 12:42:34 +0300

    1.3. JavDoc. Code convetion

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,17 @@
+/**
+* class for calcaulate task
+* 
+* @author Andrey Takmazian (takmazian@mail.ru)
+* @version 1
+* @since 10.12.2018
+*/
+
 package  ru.job4j.calculate;
 public class Calculate {
+	/**
+	* Main
+	* @param args  - args
+	*/
     public static void main(String[] args) {
 	System.out.println("Hello world");
     }
