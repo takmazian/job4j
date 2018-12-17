61ed35a0d85340d022541cb1a797a4c59ca083e8
Andrey Takmazian <takmazian@mail.ru>
2018-12-17 20:05:14 +0300

    4.1. Глупый бот.[#90756]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+/**
+ * @author Andrey Takmazyan (takmazian@mail.ru)
+        * @version $Id$
+        * @since 0.1
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) { // заменить ... на проверку, что этот вопрос известен боту и он знает как на него ответить.
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
