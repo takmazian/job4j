a8b03f38ab177094d6829780f3a5725d0bb584a7
Nikolay Ryabykh <nikolay.ryabykh@gmail.com>
2018-12-18 17:07:36 +0300

    макс трех чисел без локальной переменной

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -7,7 +7,6 @@ public class Max {
     }
 
     public int max(int first, int second, int third) {
-        int temp = max(first, second);
-        return max(temp, third);
+        return max(max(first, second), third);
     }
 }
