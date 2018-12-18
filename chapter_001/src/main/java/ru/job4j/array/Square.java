4da5eba89de15109c799d292fab24c1a89fc6f96
Nikolay Ryabykh <nikolay.ryabykh@gmail.com>
2018-12-18 19:24:15 +0300

    6.0. Заполнить массив степенями чисел.[#90770]

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+/**
+ * @author Andrey Takmazyan (takmazian@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Square {
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        for (int i = 1; i <= bound; i++) {
+            rst[i - 1] = i * i;
+        }
+        return rst;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * @author Andrey Takmazyan (takmazian@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class SquareTest {
+    @Test
+    public void whenBound3Then149() {
+        int bound = 3;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[] {1, 4, 9};
+        assertThat(rst, is(expect));
+    }
+
+    @Test
+    public void whenBound3Then14925() {
+        int bound = 5;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] expect = new int[] {1, 4, 9, 16, 25};
+        assertThat(rst, is(expect));
+    }
+}
