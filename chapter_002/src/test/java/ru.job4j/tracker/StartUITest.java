edd4e1e2897e3948a618089f6f5fe31b4e81a2f2
Andrey Takmazian <takmazian@gmail.com>
2019-05-14 16:46:55 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.[#90829]

--- chapter_002/src/test/java/ru.job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru.job4j/tracker/StartUITest.java
@@ -1,11 +1,90 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
+    @Test
+    public void whenUserAddItemThenCheckOutStream() {
+        // создаём Tracker
+        Tracker tracker = new Tracker();
+        //Напрямую добавляем заявку
+        Item item = tracker.add(new Item("test name", "desc"));
+        //создаём StubInput с последовательностью действий(производим замену заявки)
+        Input input = new StubInput(new String[]{"2", item.getId(), "test replace", "заменили заявку", "6"});
+        // создаём StartUI и вызываем метод init()
+        new StartUI(input, tracker).init();
+        // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
+        assertThat(this.out.toString(), is("Меню.\r\n" +
+                "0. Add new Item\r\n" +
+                "1. Show all items\r\n" +
+                "2. Edit item\r\n" +
+                "3. Delete item\r\n" +
+                "4. Find item by Id\r\n" +
+                "5. Find items by name\r\n" +
+                "6. Exit Program\r\n" +
+                "Элемент заменен!\r\n" +
+                "Меню.\r\n" +
+                "0. Add new Item\r\n" +
+                "1. Show all items\r\n" +
+                "2. Edit item\r\n" +
+                "3. Delete item\r\n" +
+                "4. Find item by Id\r\n" +
+                "5. Find items by name\r\n" +
+                "6. Exit Program\r\n"));
+    }
+
+    @Test
+    public void whenUserDeleteItemThenCheckOutStream() {
+        Tracker tracker = new Tracker();
+
+        Item item = tracker.add(new Item("test name", "desc"));
+        Input input = new StubInput(new String[]{"3", item.getId(), "6"});   //создаём StubInput с последовательностью действий
+        new StartUI(input, tracker).init();
+        assertThat(this.out.toString(), is("Меню.\r\n" +
+                "0. Add new Item\r\n" +
+                "1. Show all items\r\n" +
+                "2. Edit item\r\n" +
+                "3. Delete item\r\n" +
+                "4. Find item by Id\r\n" +
+                "5. Find items by name\r\n" +
+                "6. Exit Program\r\n" +
+                "Элемент был удален\r\n" +
+                "Меню.\r\n" +
+                "0. Add new Item\r\n" +
+                "1. Show all items\r\n" +
+                "2. Edit item\r\n" +
+                "3. Delete item\r\n" +
+                "4. Find item by Id\r\n" +
+                "5. Find items by name\r\n" +
+                "6. Exit Program\r\n"));
+    }
+
+
     @Test
     public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
         Tracker tracker = new Tracker();     // создаём Tracker
@@ -58,4 +137,5 @@ public class StartUITest {
         new StartUI(input, tracker).init();
         assertThat(tracker.findByName(item.getName()), is(new Item[]{tracker.findAll()[0], tracker.findAll()[1]}));
     }
+
 }
