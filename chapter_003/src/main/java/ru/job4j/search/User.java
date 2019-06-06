27b641eb464cd1e8386c70fe611e323527eb05f9
Andrey Takmazian <takmazian@gmail.com>
2019-06-06 14:50:24 +0300

    2. Написать программу преобразования List в Map.[#90824]

---
+++ chapter_003/src/main/java/ru/job4j/search/User.java
@@ -0,0 +1,11 @@
+package ru.job4j.search;
+
+public class User {
+    private Integer id;
+    private String name;
+    private String city;
+
+    public int getId(){
+        return this.id;
+    }
+}
