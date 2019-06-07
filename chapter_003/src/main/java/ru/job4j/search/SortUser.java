1930a3fc71632ded736e90cfd7730b54721f2154
Andrey Takmazian <takmazian@gmail.com>
2019-06-07 16:48:57 +0300

    1. JaCoCo. Процент покрытие тестами.[#90804]

--- chapter_003/src/main/java/ru/job4j/search/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/search/SortUser.java
@@ -12,7 +12,7 @@ public class SortUser {
         Comparator<User> comparatorUser = new Comparator<User>() {
             @Override
             public int compare(User o1, User o2) {
-                return o1.compareTo(o2);
+                return Integer.compare(o1.getName().length(), o2.getName().length());
             }
         };
         list.sort(comparatorUser);
@@ -23,13 +23,27 @@ public class SortUser {
         Comparator<User> comparatorUser = new Comparator<User>() {
             @Override
             public int compare(User o1, User o2) {
-                int result = o1.compareTo(o2);
-                return result == 0 ? Integer.compare(o1.getAge(), o2.getAge()) : result;
+                int result = 0;
+                int minLength = o1.getName().length() > o2.getName().length() ? o2.getName().length() : o1.getName().length();
+                int i;
+                for (i = 0; i < minLength; i++) {
+                    int leftSymb = o1.getName().charAt(i);
+                    int rightSymb = o2.getName().charAt(i);
+                    if (leftSymb != rightSymb) {
+                        result = Integer.compare(leftSymb, rightSymb);
+                        break;
+                    }
+                }
+                if (result == 0 && o1.getName().length() != o2.getName().length()) {
+                    result = o1.compareTo(o2);
+                }
+                return result;
             }
         };
         list.sort(comparatorUser);
         return list;
     }
+
 
 }
 
