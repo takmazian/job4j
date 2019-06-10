fefe9f72b1268f53d8e18ef7f28de68135702e10
Andrey Takmazian <takmazian@gmail.com>
2019-06-10 15:17:04 +0300

    Кофемашина. [#34741]

---
+++ coffeemachine/src/main/java/Change.java
@@ -0,0 +1,26 @@
+public class Change {
+    int[] changes(int value, int price) {
+        int[] maxCoins = new int[(value - price) / 10 + 1];
+        int remain = value - price;
+        int iter = 0;
+        while (remain > 0) {
+            if (remain >= 10) {
+                maxCoins[iter++] = 10;
+                remain-=10;
+            }
+            else if(remain >= 5){
+                maxCoins[iter++] = 5;
+                remain-=5;
+            }
+            else if(remain >= 2){
+                maxCoins[iter++] = 2;
+                remain-=2;
+            }
+            else{
+                maxCoins[iter++] = 1;
+                remain-=1;
+            }
+        }
+        return maxCoins;
+    }
+}
---
+++ coffeemachine/src/test/java/ChangeTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ChangeTest {
+    @Test
+    public void whenValueEqual50andPriceEqual35(){
+        Change change = new Change();
+        assertThat(change.changes(50,35),is(new int[]{10,5}));
+    }
+}
