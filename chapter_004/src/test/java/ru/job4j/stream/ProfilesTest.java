9a5a96a56cbbce9f33689e19737cff8a0c535a47
Andrey Takmazian <takmazian@gmail.com>
2019-06-18 16:02:19 +0300

    3. Уникальность элементов и сортировка.[#151487]

--- chapter_004/src/test/java/ru/job4j/stream/ProfilesTest.java
+++ chapter_004/src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -4,17 +4,19 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class ProfilesTest {
     @Test
-    public void testProfiles(){
-        Address address1 = new Address("Moscow","Red square",3, 12);
-        Address address2 = new Address("Moscow","Lubyanka street",2, 12);
+    public void testProfiles() {
+        Address address1 = new Address("Moscow", "Red square", 3, 12);
+        Address address2 = new Address("St. Petersburg", "Sadovaya street", 2, 12);
         Profile profile1 = new Profile(address1);
         Profile profile2 = new Profile(address2);
-        List<Profile> profileList = List.of(profile1,profile2);
-        assertTrue(Profiles.collect(profileList).containsAll(List.of(address1,address2)));
+        Profile profile3 = new Profile(address1);
+        List<Profile> profileList = List.of(profile1, profile2, profile3);
+        assertThat(Profiles.collect(profileList), is(List.of(address1, address2)));
     }
 }
