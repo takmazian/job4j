e5fb2bfa8106ec09adcb00a1489493cca91d1860
Andrey Takmazian <takmazian@gmail.com>
2019-06-19 22:01:40 +0300

    Крестики-нолики на JavaFX[#90732]

---
+++ tictactoe/src/main/java/ru/job4j/tictactoe/Figure3T.java
@@ -0,0 +1,34 @@
+package ru.job4j.tictactoe;
+
+import javafx.scene.shape.Rectangle;
+
+public class Figure3T extends Rectangle {
+    private boolean markX = false;
+    private boolean markO = false;
+
+    public Figure3T() {
+    }
+
+    public Figure3T(boolean markX, boolean markY) {
+        this.markX = markX;
+        this.markO = markY;
+    }
+
+    public Figure3T(boolean markX) {
+        this.markX = markX;
+        this.markO = !markX;
+    }
+
+    public void take(boolean markX) {
+        this.markX = markX;
+        this.markO = !markX;
+    }
+
+    public boolean hasMarkX() {
+        return this.markX;
+    }
+
+    public boolean hasMarkO() {
+        return this.markO;
+    }
+}
